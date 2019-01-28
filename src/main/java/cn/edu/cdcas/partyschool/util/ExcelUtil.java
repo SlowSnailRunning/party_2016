9f1a737ee74ce2c08be03408a4fbea8feb526529
charjin <char_jinzc@163.com>
2019-01-28 12:39:00 +0800

    add the method to get score of student and export it to output stream of excel.

--- src/main/java/cn/edu/cdcas/partyschool/util/ExcelUtil.java
+++ src/main/java/cn/edu/cdcas/partyschool/util/ExcelUtil.java
@@ -1,13 +1,13 @@
 package cn.edu.cdcas.partyschool.util;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import cn.edu.cdcas.partyschool.model.User;
+import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.*;
 
 /**
@@ -54,6 +54,78 @@ public class ExcelUtil {
         }
         workbook.close();
 
+
+        return map;
+    }
+
+    /**
+     * export students to output stream from database.
+     *
+     * @param users
+     * @param out   the output stream.
+     * @throws IOException
+     */
+    public void exportStudentScore(List<User> users, OutputStream out) throws IOException {
+        Map<Integer, List<String>> map = this.getStudentScoreMap(users);
+        List<String> header_list = map.get(-1);
+
+        int rowCount = map.size() - 1, columnCount = header_list.size();
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Info");
+        HSSFRow header = sheet.createRow(0);
+        sheet.setDefaultColumnWidth(14);
+
+        for (int i = 0; i < columnCount; i++) {
+            HSSFCell cell = header.createCell(i);
+            cell.setCellValue(header_list.get(i));
+            cell.setCellStyle(this.createHSSFCellStyle(workbook, "宋体", 10, true, BorderStyle.THIN));
+        }
+
+        HSSFCellStyle contentCellStyle = this.createHSSFCellStyle(workbook, "宋体", 10, false, BorderStyle.THIN);
+        for (int rowNum = 0; rowNum < rowCount; rowNum++) {
+            HSSFRow row = sheet.createRow(rowNum + 1);
+            List<String> row_list = map.get(rowNum);
+            for (int colNum = 0; colNum < columnCount; colNum++) {
+                HSSFCell cell = row.createCell(colNum);
+                cell.setCellValue(row_list.get(colNum));
+                cell.setCellStyle(contentCellStyle);
+            }
+        }
+
+        workbook.write(out);
+        out.close();
+        workbook.close();
+    }
+
+    /**
+     * transform the students into map.
+     *
+     * @param users students attending exam.
+     * @return
+     */
+    public Map<Integer, List<String>> getStudentScoreMap(List<User> users) {
+        Map<Integer, List<String>> map = new HashMap<>();
+        String[] header_arr = new String[]{"编号", "年级", "学院", "专业", "姓名", "学号", "初考成绩", "补考成绩"};
+
+        int rowCount = 0;
+
+        // set the key of column header to -1;
+        map.put(-1, Arrays.asList(header_arr));
+
+        // put data of users into map.
+        for (User user : users) {
+            List<String> row = new ArrayList<>();
+            row.add(0, user.getIdx() + "");
+            row.add(1, user.getGrade());
+            row.add(2, user.getDepartment());
+            row.add(3, user.getMajor());
+            row.add(4, user.getName());
+            row.add(5, user.getStudentNo());
+            row.add(6, user.getExamScore() + "");
+            row.add(7, user.getMakeUpScore() + "");
+            map.put(rowCount++, row);
+        }
 
         return map;
     }
