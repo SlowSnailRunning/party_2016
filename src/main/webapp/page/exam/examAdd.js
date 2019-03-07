dba27a5c148e24830d37cedd2e713d12f4637cc4
youlinlong <2574691263@qq.com>
2019-03-07 13:16:59 +0800

    modify something

--- src/main/webapp/page/exam/examAdd.js
+++ src/main/webapp/page/exam/examAdd.js
@@ -1,5 +1,5 @@
 layui.use(['form','layer','layedit','laydate','upload'],function(){
-    var form = layui.form
+    var form = layui.form,
     layer = parent.layer === undefined ? layui.layer : top.layer,
         laypage = layui.laypage,
         upload = layui.upload,
@@ -8,10 +8,6 @@ layui.use(['form','layer','layedit','laydate','upload'],function(){
         $ = layui.jquery;
         $('.submit-btn-submit').css({"left":"-32px","margin-left":"50%"});
         $('.examTimeRange').css("width",$('.passScore').css('width'));
-
-
-    layui.use('laydate', function() {
-        var laydate = layui.laydate;
 
         /*//日期时间选择器
         laydate.render({
@@ -31,13 +27,43 @@ layui.use(['form','layer','layedit','laydate','upload'],function(){
             elem: '#examTimeRangeId'
             ,type: 'datetime'
             ,range: true
+            ,done: function(value, date, endDate){
+                console.log(value); //得到日期生成的值，如：2017-08-18
+                console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
+                console.log(endDate); //得结束的日期时间对象，开启范围选择（range: true）才会返回。对象成员同上。
+                var str=new String();
+                var arr=new Array();
+                str=$('.examTimeRange').val();
+
+                //可以用字符或字符串分割
+                arr=str.split(' - ');
+
+                /* $('.examStartTime').val(arr[0]);
+                 $('.examEndTime').val(arr[1]);*/
+                var laydate = layui.laydate;
+                //日期时间选择器
+                laydate.render({
+                    elem: '#examStartTimeId'
+                    , type: 'datetime'
+                    ,format: 'yyyy-MM-dd HH:mm:ss'
+                    ,value:arr[0]
+                });
+                //日期时间选择器
+                laydate.render({
+                    elem: '#examEndTimeId'
+                    , type: 'datetime'
+                    ,format: 'yyyy-MM-dd HH:mm:ss'
+                    ,value:arr[1]
+                });
+                console.log($('.examStartTime').val());
+                console.log($('.examEndTime').val());
+
+                alert($('.examEndTime').val());
+
+            }
         });
-    });
 
 
-
-    layui.use('form', function () {
-        var form = layui.form;
 
 
         //监听提交
@@ -45,14 +71,14 @@ layui.use(['form','layer','layedit','laydate','upload'],function(){
             // console.log(data.field);
             // return;
             //signAdd为提交按钮的id
-            var str=new String();
+           /* var str=new String();
             var arr=new Array();
             str=$('.examTimeRange').val();
 
             //可以用字符或字符串分割
             arr=str.split(' - ');
-           /* $('.examStartTime').val(arr[0]);
-            $('.examEndTime').val(arr[1]);*/
+           /!* $('.examStartTime').val(arr[0]);
+            $('.examEndTime').val(arr[1]);*!/
 
             var laydate = layui.laydate;
 
@@ -71,7 +97,7 @@ layui.use(['form','layer','layedit','laydate','upload'],function(){
                 ,value:arr[1]
             });
             alert($('.examEndTime').val());
-
+*/
             $.post("/exam/add-update.do", data.field,
                 function (data) {
                     if (data.status === 200) {
@@ -88,7 +114,7 @@ layui.use(['form','layer','layedit','laydate','upload'],function(){
                 }, "json");
             return false;
         });
-    });
+
 
 
     //用于同步编辑器内容到textarea
