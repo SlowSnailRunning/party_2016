c6ca0ef1b1527204bc413b52b626d167f54cbbfc
hh <917479691@qq.com>
2019-03-08 17:39:24 +0800

    auth

--- src/main/java/cn/edu/cdcas/partyschool/service/impl/UserServiceImpl.java
+++ src/main/java/cn/edu/cdcas/partyschool/service/impl/UserServiceImpl.java
@@ -113,14 +113,13 @@ public class UserServiceImpl implements UserService {
     @Override
     /*需在登陆时session中设置httpSession.setAttribute("authority")*/
     public JSONResult MangerAuthorityControl(HttpSession httpSession) {
-        String authority = (String) httpSession.getAttribute("authority");
+        String authority = (String) httpSession.getAttribute("type");
         if ("ROOT".equals(authority)) {//是超级管理员，给管理员管理权限
             return new JSONResult(0, "", 0);
         } else {
             return new JSONResult(1, "", 0);
         }
     }
-
 //    @Override
 //    public int insertManger(User user) {
 //        return userMapper.insert(user);
