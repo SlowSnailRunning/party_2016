89454ccb98df3cebcc257c1b45bc75bc88fa158c
hh <3444345693@qq.com>
2019-03-21 10:36:38 +0800

    时间，修改状态

--- src/main/java/cn/edu/cdcas/partyschool/listener/Initia_par.java
+++ src/main/java/cn/edu/cdcas/partyschool/listener/Initia_par.java
@@ -13,9 +13,12 @@ public class Initia_par implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         ServletContext servletContext = servletContextEvent.getServletContext();
-        servletContext.setAttribute("php_Address","https://my.cdcas.edu.cn/core/default.php?from=iframe");
-        servletContext.setAttribute("php_login","https://my.cdcas.edu.cn");
+//        servletContext.setAttribute("php_Address","https://my.cdcas.edu.cn/core/default.php?from=iframe");
+//        servletContext.setAttribute("php_login","https://my.cdcas.edu.cn");
+        servletContext.setAttribute("php_Address", "https://172.20.253.19/core/default.php?from=iframe");
+        servletContext.setAttribute("php_login", "https://172.20.253.19");
     }
+
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
